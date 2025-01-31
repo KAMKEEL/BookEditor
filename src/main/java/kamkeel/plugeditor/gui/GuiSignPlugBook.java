b3aa3eff7e4ea63d7128734f4a22d886d98023bf
Kam <KamronGaming@gmail.com>
2025-01-31 15:22:37 -0500

    Revert "ButtonIDs Class"
    This reverts commit 9f3f4f2012249ed3f3ab1b26685e2114eaa180ec.

--- src/main/java/kamkeel/plugeditor/gui/GuiSignPlugBook.java
+++ src/main/java/kamkeel/plugeditor/gui/GuiSignPlugBook.java
@@ -1,7 +1,6 @@
 package kamkeel.plugeditor.gui;
 
 import kamkeel.plugeditor.book.Book;
-import kamkeel.plugeditor.constants.ButtonIds;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ChatAllowedCharacters;
@@ -11,14 +10,23 @@ import org.lwjgl.opengl.GL11;
 
 public class GuiSignPlugBook extends GuiScreen {
     private int bookImageWidth = 192;
+
     private int bookImageHeight = 192;
+
     private static final ResourceLocation bookGuiTextures = new ResourceLocation("textures/gui/book.png");
+
     private int updateCount = 0;
+
     private boolean titleSelected = true;
 
-    // The sign GUI now uses ButtonIds for its buttons.
+    private static final int BTN_CANCEL = 4;
+
+    private static final int BTN_FINALISE = 5;
+
     private GuiButton btnFinalise;
+
     private Book book;
+
     private GuiPlugBook parentScreen;
 
     public GuiSignPlugBook(Book _book, GuiPlugBook _parentScreen) {
@@ -26,39 +34,36 @@ public class GuiSignPlugBook extends GuiScreen {
         this.parentScreen = _parentScreen;
     }
 
-    @Override
     public void initGui() {
-        this.buttonList.add(this.btnFinalise = new GuiButton(ButtonIds.BTN_SIGN_FINALISE, this.width / 2 - 100, 4 + this.bookImageHeight, 98, 20, "Finalise"));
-        this.buttonList.add(new GuiButton(ButtonIds.BTN_SIGN_CANCEL, this.width / 2 + 2, 4 + this.bookImageHeight, 98, 20, "Cancel"));
+        this.buttonList.add(this.btnFinalise = new GuiButton(5, this.width / 2 - 100, 4 + this.bookImageHeight, 98, 20, "Finalise"));
+        this.buttonList.add(new GuiButton(4, this.width / 2 + 2, 4 + this.bookImageHeight, 98, 20, "Cancel"));
         updateButtons();
     }
 
-    @Override
     protected void actionPerformed(GuiButton buttonPressed) {
         if (!buttonPressed.enabled)
             return;
         switch (buttonPressed.id) {
-            case ButtonIds.BTN_SIGN_CANCEL:
+            case 4:
                 goBackToParentScreen();
                 break;
-            case ButtonIds.BTN_SIGN_FINALISE:
+            case 5:
                 this.book.sendBookToServer(true);
-                this.mc.displayGuiScreen(null);
+                this.mc.displayGuiScreen((GuiScreen) null);
                 break;
         }
     }
 
-    @Override
     protected void mouseClicked(int posX, int posY, int button) {
         int bookLeftSide = (this.width - this.bookImageWidth) / 2;
         int bookTextLeft = bookLeftSide + 36;
-        if (posX >= bookTextLeft && posX < bookTextLeft + 116) {
+        int bookTextTop = 33;
+        if (posX >= bookTextLeft && posX < bookTextLeft + 116)
             if (posY >= 45 && posY <= 54) {
                 this.titleSelected = true;
             } else if (posY >= 69 && posY <= 78) {
                 this.titleSelected = false;
             }
-        }
         super.mouseClicked(posX, posY, button);
     }
 
@@ -66,7 +71,6 @@ public class GuiSignPlugBook extends GuiScreen {
         this.mc.displayGuiScreen(this.parentScreen);
     }
 
-    @Override
     protected void keyTyped(char character, int keycode) {
         switch (keycode) {
             case 1:
@@ -86,23 +90,24 @@ public class GuiSignPlugBook extends GuiScreen {
                 }
                 return;
         }
-        if (ChatAllowedCharacters.isAllowedCharacter(character)) {
+        if (ChatAllowedCharacters.isAllowedCharacter(character))
             if (this.titleSelected) {
                 if (this.book.title.length() < 16)
                     this.book.title += character;
-            } else {
-                if (this.book.author.length() < 16)
-                    this.book.author += character;
+            } else if (this.book.author.length() < 16) {
+                this.book.author += character;
             }
-        }
         updateButtons();
     }
 
     private void updateButtons() {
-        this.btnFinalise.enabled = !this.book.title.isEmpty() && !this.book.author.isEmpty();
+        if (this.book.title.isEmpty() || this.book.author.isEmpty()) {
+            this.btnFinalise.enabled = false;
+        } else {
+            this.btnFinalise.enabled = true;
+        }
     }
 
-    @Override
     public void updateScreen() {
         super.updateScreen();
         this.updateCount++;
@@ -114,27 +119,31 @@ public class GuiSignPlugBook extends GuiScreen {
         this.fontRendererObj.drawString(str, bookLeftSide + 36 + (116 - strWidth) / 2, top, colour);
     }
 
-    @Override
     public void drawScreen(int par1, int par2, float par3) {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(bookGuiTextures);
         int bookLeftSide = (this.width - this.bookImageWidth) / 2;
-        byte topOffset = 2;
-        drawTexturedModalRect(bookLeftSide, topOffset, 0, 0, this.bookImageWidth, this.bookImageHeight);
-        String cursor = (this.updateCount / 10 % 2 == 0) ? EnumChatFormatting.BLACK + "_" : EnumChatFormatting.GRAY + "_";
+        byte b0 = 2;
+        drawTexturedModalRect(bookLeftSide, b0, 0, 0, this.bookImageWidth, this.bookImageHeight);
+        String cursor = "";
+        if (this.updateCount / 10 % 2 == 0) {
+            cursor = EnumChatFormatting.BLACK + "_";
+        } else {
+            cursor = EnumChatFormatting.GRAY + "_";
+        }
         String titleLine = this.book.title;
         String authorLine = this.book.author;
         if (this.titleSelected) {
-            titleLine += cursor;
+            titleLine = titleLine + cursor;
         } else {
-            authorLine += cursor;
+            authorLine = authorLine + cursor;
         }
         drawCenteredBookString("\u00a7lTitle:\u00a7r", 34, 0);
         drawCenteredBookString(titleLine, 46, 0);
         drawCenteredBookString("\u00a7lAuthor:\u00a7r", 58, 0);
         drawCenteredBookString(authorLine, 70, 0);
-        String info = "Press tab to switch between the title and author fields.\n\nNote! When you sign the book, it will no longer be editable.";
-        this.fontRendererObj.drawSplitString(info, bookLeftSide + 36, topOffset + 90, 116, 0);
+        String s3 = "Press tab to switch between the title and author fields.\n\nNote! When you sign the book, it will no longer be editable.";
+        this.fontRendererObj.drawSplitString(s3, bookLeftSide + 36, b0 + 90, 116, 0);
         super.drawScreen(par1, par2, par3);
     }
 }
