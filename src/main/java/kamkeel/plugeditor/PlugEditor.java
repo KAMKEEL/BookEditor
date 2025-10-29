e3f7dc24830f6257ec9b2bf8a9974f51a048ba60
Kam <KamronGaming@gmail.com>
2025-10-29 03:48:15 -0400

    1.4

--- src/main/java/kamkeel/plugeditor/PlugEditor.java
+++ src/main/java/kamkeel/plugeditor/PlugEditor.java
@@ -27,7 +27,7 @@ import static kamkeel.plugeditor.PlugEditor.*;
 @Mod(modid = MODID, version = VERSION, name = NAME)
 public class PlugEditor {
     public static final String MODID = "plugeditor";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
     public static final String NAME = "Plug Editor";
 
     private Minecraft mc = Minecraft.getMinecraft();
